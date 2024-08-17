d9bc7b407e7b84af390f4c978f531a3a29602ca7
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2024-08-17 01:26:16 -0300

    feat(filmes): lista de filmes js dinamica

--- level-001/html-css-fundamentos/challenge-002/js/script.js
+++ level-001/html-css-fundamentos/challenge-002/js/script.js
@@ -1,88 +1,101 @@
 
-function listagemDeFilmes() {
+const movies = [
+    {
+        image: 'https://img.elo7.com.br/product/original/3FBA809/big-poster-filme-batman-2022-90x60-cm-lo002-poster-batman.jpg',
+        title: 'Batman',
+        rating: 9.2,
+        year: 2022,
+        description: "Descrição do filme…",
+        isFavorited: true,
+    },
+    {
+        image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/9/9b/Avengers_Endgame.jpg/250px-Avengers_Endgame.jpg',
+        title: 'Avengers',
+        rating: 9.2,
+        year: 2019,
+        description: "Descrição do filme…",
+        isFavorited: false
+    },
+    {
+        image: 'https://upload.wikimedia.org/wikipedia/en/1/17/Doctor_Strange_in_the_Multiverse_of_Madness_poster.jpg',
+        title: 'Doctor Strange',
+        rating: 9.2,
+        year: 2022,
+        description: "Descrição do filme…",
+        isFavorited: false
+    },
+]
 
-    // Seleciona a div com a classe 'movies'
+
+function listagemDeFilmes(filmes) {
+
     const moviesDiv = document.querySelector('.movies');
 
-    // Cria uma nova div com a classe 'movie' e adiciona à div 'movies'
-    const moviesElement = document.createElement('div');
-    moviesElement.className = 'movie';
-    moviesDiv.appendChild(moviesElement);
+    filmes.forEach(filme => {
 
-    // Cria uma nova div com a classe 'movie-informations' e adiciona à div 'movie'
-    const movieInformationsElement = document.createElement('div');
-    movieInformationsElement.className = 'movie-informations';
-    moviesElement.appendChild(movieInformationsElement);
+        const movieElement = document.createElement('div');
+        movieElement.className = 'movie';
+        moviesDiv.appendChild(movieElement);
 
-    // Cria uma nova div com a classe 'movie-image' e adiciona à div 'movie-informations'
-    const movieImageElement = document.createElement('div');
-    movieImageElement.className = 'movie-image';
-    movieInformationsElement.appendChild(movieImageElement);
+        const movieInformationsElement = document.createElement('div');
+        movieInformationsElement.className = 'movie-informations';
+        movieElement.appendChild(movieInformationsElement);
 
-    // Cria uma nova imagem e adiciona à div 'movie-image'
-    const imageElement = document.createElement('img');
-    imageElement.src = "https://img.elo7.com.br/product/original/3FBA809/big-poster-filme-batman-2022-90x60-cm-lo002-poster-batman.jpg";
-    imageElement.alt = "Batman (2022)";
-    movieImageElement.appendChild(imageElement);
+        const movieImageElement = document.createElement('div');
+        movieImageElement.className = 'movie-image';
+        movieInformationsElement.appendChild(movieImageElement);
 
-    // Cria uma nova div com a classe 'movie-text' e adiciona à div 'movie-informations'
-    const movieTextElement = document.createElement('div');
-    movieTextElement.className = 'movie-text';
-    movieInformationsElement.appendChild(movieTextElement);
+        const imageElement = document.createElement('img');
+        imageElement.src = filme.image;
+        imageElement.alt = filme.title;
+        movieImageElement.appendChild(imageElement);
 
-    // Cria um elemento h4 e adiciona à div 'movie-text'
-    const h4Element = document.createElement('h4');
-    h4Element.textContent = 'Batman (2022)';
-    movieTextElement.appendChild(h4Element);
+        const movieTextElement = document.createElement('div');
+        movieTextElement.className = 'movie-text';
+        movieInformationsElement.appendChild(movieTextElement);
 
-    // Cria uma nova div com a classe 'rating-favorites' e adiciona à div 'movie-text'
-    const ratingFavoritesElement = document.createElement('div');
-    ratingFavoritesElement.className = 'rating-favorites';
-    movieTextElement.appendChild(ratingFavoritesElement);
+        const h4Element = document.createElement('h4');
+        h4Element.textContent = `${filme.title} (${filme.year})`;
+        movieTextElement.appendChild(h4Element);
 
-    // Cria uma nova div com a classe 'rating' e adiciona à div 'rating-favorites'
-    const ratingElement = document.createElement('div');
-    ratingElement.className = 'rating';
-    ratingFavoritesElement.appendChild(ratingElement);
+        const ratingFavoritesElement = document.createElement('div');
+        ratingFavoritesElement.className = 'rating-favorites';
+        movieTextElement.appendChild(ratingFavoritesElement);
 
-    // Cria uma nova imagem e adiciona à div 'rating'
-    const imageRatingElement = document.createElement('img');
-    imageRatingElement.src = "images/star.png";
-    imageRatingElement.alt = "Star Icon";
-    ratingElement.appendChild(imageRatingElement);
+        const ratingElement = document.createElement('div');
+        ratingElement.className = 'rating';
+        ratingFavoritesElement.appendChild(ratingElement);
 
-    // Cria um elemento span e adiciona à div 'rating'
-    const spanElement = document.createElement('span');
-    spanElement.textContent = "9.2";
-    ratingElement.appendChild(spanElement);
+        const imageRatingElement = document.createElement('img');
+        imageRatingElement.src = "images/star.png";
+        imageRatingElement.alt = "Star Icon";
+        ratingElement.appendChild(imageRatingElement);
 
-    // Cria uma nova div com a classe 'favorite' e adiciona à div 'rating-favorites'
-    const favoriteElement = document.createElement('div');
-    favoriteElement.className = 'favorite';
-    ratingFavoritesElement.appendChild(favoriteElement);
+        const spanElement = document.createElement('span');
+        spanElement.textContent = filme.rating;
+        ratingElement.appendChild(spanElement);
 
-    // Cria uma nova imagem e adiciona à div 'favorite'
-    const imageFavoriteElement = document.createElement('img');
-    imageFavoriteElement.src = "images/heart.svg";
-    imageFavoriteElement.alt = "Heart Icon";
-    favoriteElement.appendChild(imageFavoriteElement);
+        const favoriteElement = document.createElement('div');
+        favoriteElement.className = 'favorite';
+        ratingFavoritesElement.appendChild(favoriteElement);
 
-    // Cria um elemento span e adiciona à div 'favorite'
-    const spanFavoriteElement = document.createElement('span');
-    spanFavoriteElement.textContent = "Favoritar";
-    favoriteElement.appendChild(spanFavoriteElement);
+        const imageFavoriteElement = document.createElement('img');
+        imageFavoriteElement.src = "images/heart.svg";
+        imageFavoriteElement.alt = "Heart Icon";
+        favoriteElement.appendChild(imageFavoriteElement);
 
-    // Cria uma nova div com a classe 'movie-description' e adiciona à div 'movie'
-    const movieDescriptionElement = document.createElement('div');
-    movieDescriptionElement.className = 'movie-description';
-    moviesElement.appendChild(movieDescriptionElement);
+        const spanFavoriteElement = document.createElement('span');
+        spanFavoriteElement.textContent = filme.isFavorited ? "Favoritado" : "Favoritar";
+        favoriteElement.appendChild(spanFavoriteElement);
 
-    // Cria um elemento span e adiciona à div 'movie-description'
-    const spanDescriptionElement = document.createElement('span');
-    spanDescriptionElement.textContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
-    movieDescriptionElement.appendChild(spanDescriptionElement);
+        const movieDescriptionElement = document.createElement('div');
+        movieDescriptionElement.className = 'movie-description';
+        movieElement.appendChild(movieDescriptionElement);
+
+        const spanDescriptionElement = document.createElement('span');
+        spanDescriptionElement.textContent = filme.description;
+        movieDescriptionElement.appendChild(spanDescriptionElement);
+    });
 }
 
-// Chama a função para adicionar os elementos ao DOM
-
-listagemDeFilmes();
+listagemDeFilmes(movies);
