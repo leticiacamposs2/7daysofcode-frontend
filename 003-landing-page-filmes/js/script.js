2d3b86d2eca614861bf6e51701748fd3bb550d70
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2024-08-18 01:33:45 -0300

    feat(filmes): Favoritando/desfavoritando um filme e salvando no Local Storage

--- 003-landing-page-filmes/js/script.js
+++ 003-landing-page-filmes/js/script.js
@@ -1,20 +1,107 @@
 
 addEventListener('input', (event) => {
-
-    if (event?.target?.name === 'onlyFavorites') listarFilmesFavoritos();
-
-    if (event?.target?.name === 'movie-name') pesquisarFilmes();
-
+    if (event?.target?.name === 'onlyFavorites') listFavoriteMovies();
+    if (event?.target?.name === 'movie-name') searchMovies();
 });
 
 let movies = [];
 
-function listagemDeFilmes(filmes) {
+function createMovieImageElement(filme) {
+    const movieImageElement = document.createElement('div');
+    movieImageElement.className = 'movie-image';
 
+    const imageElement = document.createElement('img');
+    imageElement.src = `${BASE_URL_IMAGE}${filme.poster_path}`;
+    imageElement.alt = filme.original_title;
+    movieImageElement.appendChild(imageElement);
+
+    return movieImageElement;
+}
+
+function createMovieTextElement(filme) {
+    const movieTextElement = document.createElement('div');
+    movieTextElement.className = 'movie-text';
+
+    const h4Element = document.createElement('h4');
+    h4Element.textContent = `${filme.original_title} (${filme.vote_count})`;
+    movieTextElement.appendChild(h4Element);
+
+    const ratingFavoritesElement = createRatingFavoritesElement(filme);
+    movieTextElement.appendChild(ratingFavoritesElement);
+
+    return movieTextElement;
+}
+
+function createRatingFavoritesElement(filme) {
+    const ratingFavoritesElement = document.createElement('div');
+    ratingFavoritesElement.className = 'rating-favorites';
+
+    const ratingElement = createRatingElement(filme);
+    ratingFavoritesElement.appendChild(ratingElement);
+
+    const favoriteElement = createFavoriteElement(filme);
+    ratingFavoritesElement.appendChild(favoriteElement);
+
+    return ratingFavoritesElement;
+}
+
+function createRatingElement(filme) {
+    const ratingElement = document.createElement('div');
+    ratingElement.className = 'rating';
+
+    const imageRatingElement = document.createElement('img');
+    imageRatingElement.src = PATH_IMAGE_STAR;
+    imageRatingElement.alt = 'Star Icon';
+    ratingElement.appendChild(imageRatingElement);
+
+    const spanElement = document.createElement('span');
+    spanElement.textContent = filme.vote_average;
+    ratingElement.appendChild(spanElement);
+
+    return ratingElement;
+}
+
+function createFavoriteElement(filme) {
+    const favoriteElement = document.createElement('div');
+    favoriteElement.className = 'favorite';
+
+    const imageFavoriteElement = document.createElement('img');
+    imageFavoriteElement.src = filme.isFavorited ? PATH_IMAGE_HEART_COMPLETE : PATH_IMAGE_HEART;
+    imageFavoriteElement.alt = 'Heart Icon';
+    favoriteElement.appendChild(imageFavoriteElement);
+
+    const spanFavoriteElement = document.createElement('span');
+    spanFavoriteElement.textContent = filme.isFavorited ? 'Favoritado' : 'Favoritar';
+    favoriteElement.appendChild(spanFavoriteElement);
+
+    favoriteElement.addEventListener('click', () => {
+        filme.isFavorited = !filme.isFavorited;
+        imageFavoriteElement.src = filme.isFavorited ? PATH_IMAGE_HEART_COMPLETE : PATH_IMAGE_HEART;
+        spanFavoriteElement.textContent = filme.isFavorited ? 'Favoritado' : 'Favoritar';
+        filme.isFavorited ? saveToLocalStorage(filme) : removeFromLocalStorage(filme.id);
+    });
+
+    return favoriteElement;
+}
+
+function createMovieDescriptionElement(filme) {
+    const movieDescriptionElement = document.createElement('div');
+    movieDescriptionElement.className = 'movie-description';
+
+    const spanDescriptionElement = document.createElement('span');
+    spanDescriptionElement.textContent = filme.overview;
+    movieDescriptionElement.appendChild(spanDescriptionElement);
+
+    return movieDescriptionElement;
+}
+
+function movieListing(filmes) {
     const moviesDiv = document.querySelector('.movies');
     moviesDiv.innerHTML = '';
 
     filmes.forEach(filme => {
+
+        filme.isFavorited = checkIfFavorited(filme.id);
 
         const movieElement = document.createElement('div');
         movieElement.className = 'movie';
@@ -24,118 +111,68 @@ function listagemDeFilmes(filmes) {
         movieInformationsElement.className = 'movie-informations';
         movieElement.appendChild(movieInformationsElement);
 
-        const movieImageElement = document.createElement('div');
-        movieImageElement.className = 'movie-image';
-        movieInformationsElement.appendChild(movieImageElement);
+        movieInformationsElement.appendChild(createMovieImageElement(filme));
+        movieInformationsElement.appendChild(createMovieTextElement(filme));
 
-        const imageElement = document.createElement('img');
-        imageElement.src = `${BASE_URL_IMAGE}${filme.poster_path}`;
-        imageElement.alt = filme.original_title;
-        movieImageElement.appendChild(imageElement);
-
-        const movieTextElement = document.createElement('div');
-        movieTextElement.className = 'movie-text';
-        movieInformationsElement.appendChild(movieTextElement);
-
-        const h4Element = document.createElement('h4');
-        h4Element.textContent = `${filme.original_title} (${filme.vote_count})`;
-        movieTextElement.appendChild(h4Element);
-
-        const ratingFavoritesElement = document.createElement('div');
-        ratingFavoritesElement.className = 'rating-favorites';
-        movieTextElement.appendChild(ratingFavoritesElement);
-
-        const ratingElement = document.createElement('div');
-        ratingElement.className = 'rating';
-        ratingFavoritesElement.appendChild(ratingElement);
-
-        const imageRatingElement = document.createElement('img');
-        imageRatingElement.src = `${PATH_IMAGE_STAR}`;
-        imageRatingElement.alt = 'Star Icon';
-        ratingElement.appendChild(imageRatingElement);
-
-        const spanElement = document.createElement('span');
-        spanElement.textContent = filme.vote_average;
-        ratingElement.appendChild(spanElement);
-
-        const favoriteElement = document.createElement('div');
-        favoriteElement.className = 'favorite';
-        ratingFavoritesElement.appendChild(favoriteElement);
-
-        const imageFavoriteElement = document.createElement('img');
-        imageFavoriteElement.src = `${PATH_IMAGE_HEART}`;
-        imageFavoriteElement.alt = 'Heart Icon';
-        favoriteElement.appendChild(imageFavoriteElement);
-
-        const spanFavoriteElement = document.createElement('span');
-        spanFavoriteElement.textContent = filme.isFavorited ? 'Favoritado' : 'Favoritar';
-        favoriteElement.appendChild(spanFavoriteElement);
-
-        const movieDescriptionElement = document.createElement('div');
-        movieDescriptionElement.className = 'movie-description';
-        movieElement.appendChild(movieDescriptionElement);
-
-        const spanDescriptionElement = document.createElement('span');
-        spanDescriptionElement.textContent = filme.overview;
-        movieDescriptionElement.appendChild(spanDescriptionElement);
-
+        movieElement.appendChild(createMovieDescriptionElement(filme));
     });
 }
 
-async function getPopularFilmes() {
+async function getPopularMovies() {
     try {
         const response = await fetch(`${BASE_URL}/movie/popular?api_key=${CHAVE_API}&language=pt-BR&page=1`);
         const data = await response.json();
-        listagemDeFilmes(data.results);
+        movieListing(data.results);
     } catch (error) {
         console.error('Erro ao buscar filmes populares:', error);
     }
 }
 
 async function searchMovies(query) {
-
     const url = `${BASE_URL}/search/movie?api_key=${CHAVE_API}&query=${encodeURIComponent(query)}`;
-
     try {
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error('Erro na requisição: ' + response.statusText);
         }
-
         const data = await response.json();
         return data.results;
-
     } catch (error) {
         console.error('Foram encontrados erros durante a pesquisa do filme: ', error);
     }
 }
 
 
-function pesquisarFilmes() {
-
+function searchMovies() {
     const pesquisa = document.getElementById('movie-name');
     const movieDiv = document.querySelector('.movie');
+    if (!pesquisa.value && movieDiv == null) return getPopularMovies();
 
-    if (!pesquisa.value && movieDiv == null) return getPopularFilmes();
-
-    searchMovies(pesquisa.value).then(movies => listagemDeFilmes(movies));
-
+    searchMovies(pesquisa.value).then(movies => movieListing(movies));
 }
 
-function listarFilmesFavoritos() {
-
+function listFavoriteMovies() {
     const checkbox = document.getElementById('onlyFavorites');
-
-    if (checkbox.checked) {
-
-        console.log('selecionado');
-
-    } else {
-
-        console.log('não selecionado');
-    }
-
-
+    checkbox.checked ? movieListing(getLocalStorage()) : getPopularMovies();
 }
 
-getPopularFilmes()
+function saveToLocalStorage(movie) {
+    const filmes = getLocalStorage();
+    filmes.push(movie);
+    localStorage.setItem("filmes-favoritos", JSON.stringify(filmes));
+}
+
+function removeFromLocalStorage(movieId) {
+    const filmes = getLocalStorage();
+    const updatedFilmes = filmes.filter(filme => filme.id !== movieId);
+    localStorage.setItem("filmes-favoritos", JSON.stringify(updatedFilmes));
+}
+
+function checkIfFavorited(movieId) {
+    const filmes = getLocalStorage();
+    return filmes.some(filme => filme.id === movieId);
+}
+
+function getLocalStorage() { return JSON.parse(localStorage.getItem("filmes-favoritos")) || []; }
+
+getPopularMovies()
